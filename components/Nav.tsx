4dbb2a03493f695747b36efd68350e28d2bf0dff
Anthony Cavuoti <anthonycavuoti@gmail.com>
2023-01-07 16:50:51 -0800

    Add implimation to main sit settings control

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,18 +1,8 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 
 interface Props {
   /**
@@ -20,6 +10,7 @@ interface Props {
    * You won't need it on your project.
    */
   window?: () => Window;
+  siteName: string;
 }
 
 export default function DrawerAppBar(props: Props) {
@@ -32,7 +23,7 @@ export default function DrawerAppBar(props: Props) {
             component="div"
             sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
           >
-            Humboldt Life
+            {props.siteName}
           </Typography>
         </Toolbar>
       </AppBar>
